7420f6a93c35285e39db34bff5973a314fb155dd
Alvaro Martin Caballero <a.martin@lhc.host>
2022-07-29 15:39:08 -0500

    eliminando firebase

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,13 +1,3 @@
-export const firebase = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
-};
-
 export const contentful = {
   space: process.env.REACT_APP_CONTENTFUL_SPACE,
   access_token: process.env.REACT_APP_CONTENTFUL_ACCESS_TOKEN,
