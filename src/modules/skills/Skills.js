1c0cce8e1700f8f2ca2ef6c17f351288c624e8bc
Alvaro Martin Caballero <a.martin@lhc.host>
2023-02-16 18:03:54 -0500

    quick fix

--- src/modules/skills/Skills.js
+++ src/modules/skills/Skills.js
@@ -1,4 +1,4 @@
-import { Box, Grid, Typography } from "@mui/material";
+import { Box, Chip, Grid, Typography } from "@mui/material";
 import React from "react";
 
 import skillsConfig from "../../utils/skills";
@@ -9,7 +9,9 @@ const Skills = () => {
       <Box>
         {skillsConfig.map((skillset) => (
           <Box key={skillset.set}>
-            <Typography fontWeight={"bold"}>{skillset.set}</Typography>
+            <Typography fontWeight={"bold"} align="center">
+              {skillset.set}
+            </Typography>
             <Grid container>
               {skillset.skills.map((skill) => (
                 <Grid
@@ -35,7 +37,8 @@ const Skills = () => {
                       alignItems={"center"}
                       width={100}
                       height={100}
-                      bgcolor="rgba(255,255,255,0.5)"
+                      bgcolor="rgba(255,255,255,0.3)"
+                      border={"1px solid"}
                     >
                       <Box>
                         <img src={skill.icon} alt={skill.name} height={30} />
